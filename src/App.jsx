3783ec1540d442a85951d3fe08abb2b10a072a9c
Rajesh-snippet <rsarmabordoloi@gmail.com>
2026-08-16 13:13:45 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="min-h-screen bg-zinc-950 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-white">
-        Rajesh's Portfolio
-      </h1>
-    </div>
-  )
+    <>
+      <Navbar />
+
+      <main id="home" className="min-h-screen bg-zinc-950 pt-20">
+        <div className="flex min-h-screen items-center justify-center">
+          <h1 className="text-4xl font-bold text-white">
+            Rajesh's Portfolio
+          </h1>
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
